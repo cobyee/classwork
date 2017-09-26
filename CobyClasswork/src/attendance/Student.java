652a93f26414c554bcec629a887e04adeccc4436
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-09-26 11:46:15 -0400

    Started Student

---
+++ CobyClasswork/src/attendance/Student.java
@@ -0,0 +1,29 @@
+package attendance;
+
+public class Student(first,last) implements Attendee{
+
+	private String firstName = this.first;
+	public boolean isPresent() {
+		
+	}
+
+	public void setPresent(boolean present) {
+		
+	}
+
+	public String getFirstName() {
+		return first;
+	}
+
+	public String getLastName() {
+		return last;
+	}
+
+	public boolean mathces(String first, String last) {
+		
+	}
+
+	public boolean matches(String last) {
+	
+	}
+}
