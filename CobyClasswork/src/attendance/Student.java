d9c65d75e6ffc17cefc4864f10d9495791bf8b81
Coby <Coby@Coby-PC.T-mobile.com>
2017-09-26 23:15:12 -0400

    Finish Student

--- CobyClasswork/src/attendance/Student.java
+++ CobyClasswork/src/attendance/Student.java
@@ -1,29 +1,70 @@
 package attendance;
 
-public class Student(first,last) implements Attendee{
+public class Student implements Attendee{
 
-	private String firstName = this.first;
+	private String firstName;
+	private String lastName;
+	private boolean present = false;
+	
+	public Student(String first, String last) {
+		this.firstName = first;
+		this.lastName = last;
+	}
 	public boolean isPresent() {
-		
+		return present;
 	}
 
 	public void setPresent(boolean present) {
-		
+		this.present = present;
 	}
 
 	public String getFirstName() {
-		return first;
+		return firstName;
 	}
 
 	public String getLastName() {
-		return last;
+		return lastName;
 	}
 
 	public boolean mathces(String first, String last) {
-		
+		if(first.equals(firstName)) {
+			if(last.equals(lastName))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public boolean matches(String last) {
+		if(last.equals(lastName)) {
+			return true;
+		}
+		return false;
+	}
 	
+	public String getReportString() {
+		String line = lastName;
+		if(lastName.length() > 20) {
+			if(firstName.length() > 20) {
+				line = lastName.substring(0,17) + "..." + firstName.substring(0,17) + "..." + isPresent();
+				return line;
+			}
+		}
+		String r = line;
+		while(r.length() < 20) {
+			r += " ";
+		}
+		r += firstName;
+		while(r.length() < 40) {
+			r += " ";
+		}
+		if(present) {
+			r += "Present" + "\n";
+		}
+		else {
+			r += "Absent" + "\n";
+		}
+		return r;
 	}
 }
