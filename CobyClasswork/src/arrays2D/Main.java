d5e8bed76440d79f92bfa45b24d0b6fc6da6c7df
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-10-20 11:48:08 -0400

    a

--- CobyClasswork/src/arrays2D/Main.java
+++ CobyClasswork/src/arrays2D/Main.java
@@ -10,6 +10,9 @@ public class Main {
 	 * increases the element in arr at index psn
 	 * and decreases the elements at psn-1 and psn+1
 	 * if they exit
+	 * 
+	 * THIS idea is another major topic in AP CSA
+	 * It is the idea of avoiding an ArrayIndexOutOfBounds exceptions
 	 * @param arr
 	 * @param psn
 	 */
