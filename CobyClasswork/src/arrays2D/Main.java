c669923f0a836fe6d5c7c479c44e0a849333a11f
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-10-20 11:47:05 -0400

    a

---
+++ CobyClasswork/src/arrays2D/Main.java
@@ -0,0 +1,25 @@
+package arrays2D;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * increases the element in arr at index psn
+	 * and decreases the elements at psn-1 and psn+1
+	 * if they exit
+	 * @param arr
+	 * @param psn
+	 */
+	public static void changeNeigbors(int[] arr, int psn) {
+		arr[psn] = arr[psn]+1;
+		if(psn>0) {
+			arr[psn-1] = arr[psn-1]-1;
+		}
+		if(psn<arr.length-1) {
+			arr[psn+1] = arr[psn+1]+1;
+		}
+	}
+}
